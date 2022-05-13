75f1b31b5855507308153f18d0a17576d1289da8
jalal <websw98@gmail.com>
2022-05-13 11:34:27 +0430

    init project and create otp model and migration

---
+++ database/migrations/2022_05_13_070250_create_otps_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('otps', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('mobile');
+            $table->string('otp');
+            $table->timestamp('expire_otp_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otps');
+    }
+};
